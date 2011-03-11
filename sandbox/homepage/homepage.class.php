240cd63157a5096560fa7ea11a0cc8eddcb6ab82
namnyam <namnyam@92398ba2-4b84-509a-b2d1-1602ea4e950d>
2011-03-11 07:18:52 +0000

    homepage 모듈 생성시 카페 생성

--- sandbox/homepage/homepage.class.php
+++ sandbox/homepage/homepage.class.php
@@ -16,6 +16,60 @@
             $oModuleController->insertTrigger('display', 'homepage', 'controller', 'triggerMemberMenu', 'before');
 			$oModuleController->insertTrigger('moduleHandler.proc', 'homepage', 'controller', 'triggerApplyLayout', 'after');
 
+			/* 신규 카페 생성 시작 2011 03 11*/
+			// 카페레이아웃 생성
+            $args->site_srl = 0;
+            $args->layout_srl = getNextSequence();
+            $args->layout = 'xe_cafe';
+            $args->title = 'CafeXE default layout';
+			$args->layout_type = "P";
+
+            // DB 입력
+            $output = $this->insertLayout($args);
+            if(!$output->toBool()) return $output;
+
+            $oLayoutAdminController = &getAdminController('layout');
+            $this->initLayout($args->layout_srl, $args->layout);
+
+			$layout_srl = $args->layout_srl;
+
+			unset($args);
+
+            // 결과 리턴
+            $oHomepageAdminController = &getAdminController('homepage');
+            $oHomepageModel = &getModel('homepage');
+            $oModuleModel = &getModel('module');
+            $oMemberModel = &getModel('member');
+            $oMemberController = &getController('member');
+
+            $cafe_id = 'sample_cafe';
+            $cafe_title = 'sample cafe입니다.';
+            $cafe_description = 'Cafe XE 샘플 입니다.';
+
+            $domain = $cafe_id;
+
+            $oHomepageAdminController->insertHomepage($cafe_title, $domain);
+            if(!$oHomepageAdminController->toBool()) return $output;
+
+            $site_srl = $oHomepageAdminController->get('site_srl');
+
+            // 홈페이지 제목/내용 변경
+            $homepage_info = $oHomepageModel->getHomepageInfo($site_srl);
+            $args->title = $cafe_title;
+            $args->description = $cafe_description;
+            $args->layout_srl = $layout_srl;
+            $args->site_srl = $site_srl;
+            $output = executeQuery('homepage.updateHomepage', $args);
+            if(!$output->toBool()) return $output;
+
+            // 현재 사용자 가입 및 관리자 주기
+            $logged_info = Context::get('logged_info');
+
+            $default_group = $oMemberModel->getDefaultGroup($site_srl);
+            $oMemberController->addMemberToGroup($logged_info->member_srl, $default_group->group_srl, $site_srl);
+
+            $output = $oModuleController->insertSiteAdmin($site_srl, array($logged_info->user_id));
+
             return new Object();
         }
 
