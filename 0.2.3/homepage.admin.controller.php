c07adadb0c350f717e42b82e02c1f198579cc2ef
chschy@gmail.com <chschy@gmail.com@92398ba2-4b84-509a-b2d1-1602ea4e950d>
2011-10-26 02:49:16 +0000

    issue 24, modify default skin, and manage.html tpl

--- 0.2.3/homepage.admin.controller.php
+++ 0.2.3/homepage.admin.controller.php
@@ -189,7 +189,7 @@
             $oModuleAdminController->makeCacheDefinedLangCode($info->site_srl);
 
             $homepage_config = $oHomepageModel->getConfig(0);
-            if(!$homepage_config->default_layout) $homepage_config->default_layout = 'xe_cafe';
+            if(!$homepage_config->default_layout) $homepage_config->default_layout = 'xe_cafe_site';
 
             // 레이아웃 생성
             $info->layout_srl = $this->makeLayout($info->site_srl, $title,$homepage_config->default_layout);
